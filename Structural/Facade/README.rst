eaceed4cbc490bd37a83a4e2514392849e688327
abdelrahman badr <abdelrahmanbadr@abdelrahmans-MacBook-Pro.local>
2018-09-29 20:16:03 +0200

    facade design pattern in go

---
+++ Structural/Facade/README.rst
@@ -0,0 +1,28 @@
+Facade
+==========
+
+Purpose
+-------
+Facade pattern hides the complexities of the system.
+
+The primary goal of a Facade Pattern is not to avoid you having to read the
+manual of a complex API. It's only a side-effect. The first goal is to
+reduce coupling and follow the Law of Demeter.
+
+A Facade is meant to decouple a client and a sub-system by embedding
+many (but sometimes just one) interface, and of course to reduce
+complexity.
+
+-  A facade does not forbid you the access to the sub-system
+-  You can (you should) have multiple facades for one sub-system
+
+
+UML Diagram
+-----------
+
+.. image:: uml/facade.jpg
+   :alt: Alt Facade UML Diagram
+   :align: center
+
+Test
+----
---
+++ Structural/Facade/facade.go
@@ -0,0 +1,65 @@
+package main
+
+import (
+	"fmt"
+)
+
+const (
+	BOOT_ADDRESS = 0
+	BOOT_SECTOR  = 0
+	SECTOR_SIZE  = 1023
+)
+
+type CPU struct{}
+
+func (c *CPU) Freeze() {
+	fmt.Println("CPU frozen")
+}
+
+func (c *CPU) Jump(position int) {
+	fmt.Println("CPU jumps to", position)
+}
+
+func (c *CPU) Execute() {
+	fmt.Println("CPU executes")
+
+}
+
+type Memory struct{}
+
+func (this *Memory) Load(position int64, data []byte) {
+	fmt.Println("Memory loads")
+}
+
+type HardDrive struct{}
+
+func (hd *HardDrive) Read(lba int, size int) []byte {
+	fmt.Println("HardDrive reads")
+	return make([]byte, 0)
+}
+
+type Computer struct {
+	cpu       *CPU
+	memory    *Memory
+	hardDrive *HardDrive
+}
+
+func NewComputer() *Computer {
+	cpu := new(CPU)
+	memory := new(Memory)
+	hardDrive := new(HardDrive)
+	return &Computer{cpu: cpu, memory: memory, hardDrive: hardDrive}
+
+}
+
+func (this *Computer) Start() {
+	this.cpu.Freeze()
+	this.memory.Load(BOOT_ADDRESS, this.hardDrive.Read(BOOT_SECTOR, SECTOR_SIZE))
+	this.cpu.Jump(BOOT_ADDRESS)
+	this.cpu.Execute()
+}
+
+func main() {
+	computer := NewComputer()
+	computer.Start()
+}
